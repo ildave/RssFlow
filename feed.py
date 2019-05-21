c1801dd105cf3297bd462da1fe21d5d4bc550284
Davide Gualano <ildave@gmail.com>
2019-05-21 16:22:28 +0200

    Add title attribute to Feed

--- feed.py
+++ feed.py
@@ -1,8 +1,9 @@
 class Feed:
-    def __init__(self, link, description=None, updated=None, feedurl=None, feedtitle=None, feedid=None):
+    def __init__(self, link, description=None, updated=None, feedurl=None, feedtitle=None, feedid=None, title=None):
         self.link = link
         self.description = description
         self.updated = updated
         self.feedurl = feedurl
         self.feedtitle = feedtitle
         self.feedid = feedid
+        self.title = title
