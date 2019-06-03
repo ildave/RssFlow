1ec05c9913e58c2168a054703d21f4e21cc4b16e
Davide Gualano <ildave@gmail.com>
2019-06-03 17:39:04 +0200

    add method to Feed object to make it serializable

--- feed.py
+++ feed.py
@@ -7,3 +7,14 @@ class Feed:
         self.feedtitle = feedtitle
         self.feedid = feedid
         self.title = title
+
+    def to_dict(self):
+        out = {}
+        out['link'] = self.link
+        out['description'] = self.description
+        out['updated'] = self.updated
+        out['feedurl'] = self.feedurl
+        out['feedtitle'] = self.feedtitle
+        out['feedid'] = self.feedid
+        out['title'] = self.title
+        return out
