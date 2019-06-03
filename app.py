5e9b02611fa9a8677124ffab8687379c3379f17a
Davide Gualano <ildave@gmail.com>
2019-06-03 17:39:24 +0200

    add main webapp file

--- app.py
+++ app.py
@@ -2,27 +2,26 @@ import asyncio
 import websockets
 import logging
 import queue
+import json
+from refresher import Refresher
 
-async def send_items(websockets, path):
 
-
-async def hello(websocket, path):
+async def send_items(websocket, path):
+    global data_queue
     while True:
         print('§', flush=True)
-        self.logger.info("Run - {} items".format(self.queue.qsize()))
-        if not self.queue.empty():
-
-            item = self.queue.get()
-            self.show(item)
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
+        if not data_queue.empty():
+            item = data_queue.get()
+            print(json.dumps(item.to_dict()))
+            await websocket.send(json.dumps(item.to_dict()))
+        await asyncio.sleep(60)
 
 data_queue = queue.Queue()
 refresher = Refresher(data_queue, logging.getLogger('refresher'))
 refresher.start()
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+start_server = websockets.serve(send_items, '127.0.0.1', 8066)
+
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
